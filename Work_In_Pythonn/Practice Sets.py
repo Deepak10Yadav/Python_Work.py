0ca8774ffed9c79ead27cf5814ccddc69e70a1cb
Deepak Yadav <dy60146@gmail.com>
2025-05-07 23:40:10 +0530

    numpy practice

--- Work_In_Pythonn/Practice Sets.py
+++ Work_In_Pythonn/Practice Sets.py
@@ -218,7 +218,7 @@
 
 # from numpy import *
 # print(sum(range(5),-1))
-
+# print(sum(range(5)))
 
 # '''python
 # Z**Z
@@ -230,3 +230,136 @@
 # '''
 # import numpy  as np
 # Z = np.array([]) 
+
+
+# np.array(0) / np.array(0)
+
+# np.array(0) // np.array(0)
+# np.array([np.nan]).astype(int).astype(float)
+
+
+
+# import numpy as np
+# arr = np.array([1.2, -1.2, 2.5, -2.5, 0.0])
+# rounded = np.where(arr > 0, np.ceil(arr), np.floor(arr)) 
+#np.ceil() rounds up toward positive infinity.
+#np.floor() rounds down toward negative infinity.
+# np.where() applies the correct function based on the sign of each number.
+
+
+
+# import numpy as np
+# a = np.array([1,2,3,4,5])
+# b = np.array([6,5,8,9,10])
+
+# common = np.intersect1d(a,b) # to find common value  of any array we use np.intersect1d it finds  the intersection value of the arrays.
+# print(common)
+
+# 33. How to get the dates of yesterday, today and tomorrow?
+# Answer:
+# import numpy as np
+# from datetime import date,timedelta
+
+# today = date.day
+# print(today)
+
+# 35. How to compute ((A+B)*(-A/2)) in place (without copy)? (★★☆)
+
+# np.sqrt(-1) == np.emath.sqrt(-1)
+
+# 37. Create a 5x5 matrix with row values ranging from 0 to 4
+
+# import numpy as np
+# matrix = np.array(([0,2,3,4,5],
+#                    [1,6,4,3,7],
+#                    [2,7,8,4,6],
+#                    [3,8,0,4,2],
+#                    [4,7,8,5,9]))
+# print(matrix)
+
+#38. Consider a generator function that generates 10 integers and use it to build an array (★☆☆)
+# import numpy as np
+# s = int(input("Enter The Number:"))
+# def generator():
+#     a= np.random.random(s)
+#     print(a)
+
+# b = np.array([(generator)])
+# print(b)
+# generator()
+
+
+import numpy as np
+
+# Create 10 evenly spaced values between 0 and 1, excluding both endpoints
+# vector = np.linspace(0, 1, 12)[1:-1]
+# print(vector)
+
+# method - 2
+# vector1 = np.random.uniform(low=0.1,high=0.9,size=10)
+# print(vector1)
+
+#### 40. Create a random vector of size 10 and sort it (★★☆)
+
+# vector = np.random.random(10)
+# vector.sort()
+# print(vector)
+
+#### 42. Consider two random arrays A and B, check if they are equal (★★☆)
+
+# a = np.random.random((3))
+# b = np.random.random((3))
+# print(a)
+# print(b)
+# print(a==b)
+
+#### 43. Make an array immutable (read-only) (★★☆)
+
+# arr1 = np.ones([6])
+# arr1.setflags(write=False) # it will run untill any operations makes an error.after error it will give you ValueError.
+# arr1[2] = 3 # here we are trying to change the value of array := arr1  aat position 2 and it is giving the error becz now the array is immutabe.
+# print(arr1)
+
+#### 44. Consider a random 10x2 matrix representing cartesian coordinates, convert them to polar coordinates (★★☆)
+
+# arr2 = np.random.random((10,2))
+# print("array1:",arr2)
+# arr3 = np.random.random((10,2))
+# print("array2:",arr3)
+# # r = √(x² + y²) formula to convert cartesian to polar.
+# square1 = np.square(arr2,arr3) #(x² + y²)
+# print("sqaure:",square1)
+# formula = np.sqrt(square1) #√(x² + y²)
+# print("polar:",formula)
+
+
+#### 45. Create random vector of size 10 and replace the maximum value by 0 (★★☆)
+
+# vector=np.random.random(10)
+# vector[vector.argmax()]=0
+# print(vector)
+
+
+# 49. How to print all the values of an array? (★★☆)
+# import sys
+# array1 = np.ones([4,5])
+# print(array1)
+
+#### 50. How to find the closest value (to a given scalar) in a vector?
+
+# vec = np.array(([0.1,0.5,0.7,0.8,0.4,0.2]))
+# scl = 1.0
+# a = np.abs(vec - scl).argmin()
+# vec1=vec[a]
+# print(vec1)
+
+#### 51.Create a structured array representing a position (x,y) and a color (r,g,b) (★★☆)
+import matplotlib.pyplot as plt
+x = np.array([1,2,3])
+y = np.array([4,5,7])
+color = ['red','green','blue']
+plt.scatter(x,y,c=color)
+plt.show()
+print(x)
+print(y)
+
